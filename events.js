db1177f183f580749d216eaaf3c36bed43f28814
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-09-23 23:29:54 +0530

    OnClick Events with buttons

---
+++ events.js
@@ -0,0 +1,24 @@
+// option - 2
+function makeYellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+// option -3
+const blueButton = document.getElementById("make-blue");
+blueButton.onclick = makeBlue;
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+// option - 4
+const greenButton = document.getElementsByClassName("make-green");
+// console.log(greenButton);
+greenButton[0].onclick = function makeGreen() {
+  document.body.style.backgroundColor = "green";
+};
+
+// option - 5
+const purpleButton = document.getElementById("make-purple");
+purpleButton.onclick = function makePurple() {
+  document.body.style.backgroundColor = "purple";
+};
